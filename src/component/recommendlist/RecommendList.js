322137c00e877df6e1db67ad2ff345fa092db8f4
pycainiao <940761416@qq.com>
2020-09-11 15:41:07 +0800

    feat(新增推荐功能,修复不能滚动的问题):

--- src/component/recommendlist/RecommendList.js
+++ src/component/recommendlist/RecommendList.js
@@ -1,18 +1,25 @@
 import React from "react";
+import style from "./restyle.module.less";
 
 function RecommendList(props) {
   const { recommendList } = props;
   return (
     <>
-      <h1 className="title">推荐歌单</h1>
-
-      {recommendList.map((item) => {
-        return (
-          <div key={item.id} className={"wrapper"}>
-            <div className="desc">{item.name}</div>
-          </div>
-        );
-      })}
+      <h1 className={style["title"]}>推荐歌单</h1>
+      <div className={style["container"]}>
+        {recommendList.map((item) => {
+          return (
+            <div key={item.id} className={style["wrapper"]}>
+              <div className={style["play_count"]}>
+                <i className="iconfont play">&#xe885;</i>
+                <span>{Math.floor(item.playCount / 10000)}万</span>
+              </div>
+              <img src={item.picUrl} alt="logo" />
+              <div className={style["desc"]}>{item.name}</div>
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 }
--- src/view/recommend/Recommend.js
+++ src/view/recommend/Recommend.js
@@ -1,35 +1,45 @@
-import React, { useRef, useEffect } from "react";
-import { getBannerList, getRecommendList } from "./store/actionCreators";
-import { connect } from "react-redux";
+import React, {useEffect, useRef, useState} from "react";
+import {getBannerList, getRecommendList} from "./store/actionCreators";
+import {connect} from "react-redux";
 import BScroll from "better-scroll";
 import Slider from "@/component/slider/slider";
 import RecommendList from "@/component/recommendlist/RecommendList";
 import style from "./style/recommend.module.less";
+
 function Recommend(props) {
   const { bannerList, recommendList } = props;
   const { getBannerListDispatch, getRecommendListDispatch } = props;
   const reMain = useRef();
+  const [bScroll, setBScroll] = useState();
   useEffect(() => {
     // 可以传递的是一个原生的DOM 对象
-    // eslint-disable-next-line no-unused-vars
-    const scroll = new BScroll(reMain.current, {
-      scrollY: true,
-    });
+    const scroll = new BScroll(reMain.current, {});
     if (bannerList.length === 0) {
       // 获取轮播图列表
       getBannerListDispatch();
     }
     !recommendList.length && getRecommendListDispatch(); // 推荐列表
-
+    setBScroll(scroll);
+    // setTimeout(() => {
+    //   scroll.refresh(); // 需要重新刷新,因为,还没获取玩数据,这个BScroll 已经初始化完了 带后续完善下这个
+    // }, 5000);
+    return () => {
+      setBScroll(null);
+    };
     // eslint-disable-next-line
   }, []);
+  useEffect(() => {
+    if (bScroll) {
+      console.log("刷新滚动");
+      bScroll.refresh(); // 因为数据是后获取到的,因此需要刷新,重新计算才行,不然,滚动不会生效的
+    }
+  });
   return (
-    <div className={style["re-main"]} ref={reMain}>
+    <div className={style["re-main"]} ref={reMain} id="wrapper">
       {/*下面这个div是必须的,不然无法出现滚动,这是出现滚动的条件导致的*/}
       <div>
         <Slider bannerList={bannerList} />
         <RecommendList recommendList={recommendList} />
-        {/*<div style={{ height: "1000px" }}>1</div>*/}
       </div>
     </div>
   );
